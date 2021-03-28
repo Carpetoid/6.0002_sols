25cb9cb680495872a1f39ace7a869943cf65467b
Carpetoid <abdulrahmanfayoumi2@gmail.com>
2021-03-28 20:33:32 +0300

    pset1b

--- PS1/ps1b.py
+++ PS1/ps1b.py
@@ -22,9 +22,22 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     
     Returns: int, smallest number of eggs needed to make target weight
     """
-    # TODO: Your code here
-    pass
-
+     # TODO: Your code here
+    newegg = []
+    eggs = 0
+    avail = target_weight
+    
+    for i in range(len(egg_weights)):
+        newegg.append(egg_weights[((len(egg_weights) - 1) - i)])
+       
+    
+    for i in newegg:
+        eggs += avail // i
+        avail -= (avail // i) * i
+    return eggs
+        
+     
+ 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
     egg_weights = (1, 5, 10, 25)
@@ -32,5 +45,19 @@ if __name__ == '__main__':
     print("Egg weights = (1, 5, 10, 25)")
     print("n = 99")
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
+    print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+    egg_weights = (2, 3, 5, 10, 50)
+    n = 998
+    print("Egg weights = (2, 3, 5, 50)")
+    print("n = 998")
+    print("Expected ouput: 25 (19 * 50 + 4 * 10 + 1 * 5 + 1 * 3 = 998)")
+    print("Actual output:", dp_make_weight(egg_weights, n))
+    print()
+    egg_weights = (3, 5, 10, 20)
+    n = 543
+    print("Egg weights = (3, 5, 10, 20)")
+    print("n = 545")
+    print("Expected ouput: 28 (27 * 20 + 1 * 5 = 28)")
     print("Actual output:", dp_make_weight(egg_weights, n))
     print()
